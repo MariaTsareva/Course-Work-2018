debad908892dab8d939ee66637f60ac3763661e8
MariaTsareva <ladymary9627@gmail.com>
2018-05-20 11:00:55 +0300

    Update freq2.py

--- freq2.py
+++ freq2.py
@@ -36,7 +36,6 @@ def count_text(text1):
     for word in words:
         count_1 += 1
         stats[word] = stats.get(word, 0) + 1
-    print('Количество слов: ', count_1)
     return words, count_1
 
 
@@ -44,15 +43,11 @@ def del_part(words):
     from pymorphy2 import MorphAnalyzer
     morph = MorphAnalyzer()
     clean_words = []
-    # print('3.1')
     words1 = [item for item in words if not item.isdigit()]
-    # print('3.2')
     for word in words1:
         ana = morph.parse(word)
-        #print('3.3')
         first = ana[0]
         if first.tag.POS == 'PREP' or first.tag.POS == 'CONJ'or first.tag.POS == 'PRCL' :
-            # print('3.4')
             continue
         else:
             if first.word in 'abcdefghijklmnopqrsuvwxyzóáéíã':
@@ -60,7 +55,6 @@ def del_part(words):
                 continue
             else:
                 clean_words.append(first.word)
-    # print(clean_words)
     return clean_words
 
 
@@ -85,15 +79,12 @@ def clean_words1(clean_words, count_1):
     my_words = pd.DataFrame(
         {'word': word_,
          'my ipm': ipm_})
-    print(type(my_words['word']))
     return my_words
 
 
 def dict_pd():
     a = pd.read_csv('C://Put_txt/freqrnc2011.csv', sep='\t')
     a['Lemma'].str.lower()
-    print(len(a))
-    # print(a)
     return a
 
 
@@ -106,15 +97,12 @@ def comparison(my_words, a):
     col = col.reset_index(drop=True)
     col = col.sort_values('word')
     col1 = a[a.Lemma.isin(my_words.word)]
-    print(len(col))
     col1 = col1.drop_duplicates('Lemma')
     col1 = col1.reset_index(drop=True)
     col1 = col1.sort_values('Lemma')
     col1 = col1.rename(index=str, columns={"Lemma": "word"})
-    print(len(col1))
     col2 = col.merge(col1, how='left', on='word')
     col2 = col2.drop(['PoS', 'R', 'D', 'Doc'], axis=1, inplace=False)
-    print(col2)
     col2.to_csv('C://Put_txt/my_database.csv', index=None)
     return col2
 
@@ -128,13 +116,9 @@ def lexicon(col2):
 
 
 new_file = open_file()
-print('1')
 converce = from_str_to_list(new_file)
-print('2')
 counting, my_count = count_text(converce)
-print('3')
 delin = del_part(counting)
-print('4')
 cleaning = clean_words1(delin, my_count)
 frec2011 = dict_pd()
 my_compare = comparison(cleaning, frec2011)
